8207d9dcbb34ebabc4a277c9d46a5fb12f3b922a
Mark Varadi <Mark.Varadi@cloudflight.io>
2020-06-05 15:11:27 +0300

    custom directives added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule} from '@angular/forms';
 
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BasicHighlightDirective} from "./basic-highlight/basic-highlight.directive";
+import {BetterHighlightDirective} from './better-highlight/better-highlight.directive';
+import {UnlessDirective} from './unless/unless.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BasicHighlightDirective,
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
@@ -16,4 +22,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
